b3eb9979d9bad0b0a4f79fce62becfef2889c2ef
Siddharth jain <162619604+godxcodex@users.noreply.github.com>
2025-08-08 22:53:57 +0530

    List.of

--- List/Array_list.java
+++ List/Array_list.java
@@ -1,7 +1,19 @@
 
 package List;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 public class Array_list {
-    List<Integer> list = new ArrayList<>();
-    List <Integer> list1 = List.of(1,33,44);
+    public static void main(String[] args) {
+        
+        List<Integer> list = new ArrayList<>();
+        list.add(22);
+        list.add(34);
+        list.add(22);
+        list.add(33);
+        System.out.println(list);
+        List <Integer> list1 = List.of(1,33,44);// can change in this means can not add // change//or modifide if Using --> List.of 
+        list.addAll(list1);// Use to add the list elements in a list // 
+        list.sort(null);
+        System.out.println(list);
+    }
 }
