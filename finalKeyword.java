75b45c5a1c79f27cd6d7ee6eeda06084e6671f82
Siddharth jain <162619604+godxcodex@users.noreply.github.com>
2025-08-15 23:10:39 +0530

    static  and final

---
+++ finalKeyword.java
@@ -0,0 +1,8 @@
+public class finalKeyword {
+    public static void main(String[] args) {
+        final int k = 90;
+        System.out.println(k);
+        
+    }
+    // final keyword ka use asa ha ki wo value ko final kar deta ha us variable ki aur usko koi change nahi kar sakta ...!//
+}    // Eg if  final int x =90 aur ma x ko asa karu x+=78; to ye error de ga //
