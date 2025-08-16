f43449bcd39aca6fd69af44b8023b7d089096ea0
Siddharth jain <162619604+godxcodex@users.noreply.github.com>
2025-08-16 22:43:18 +0530

    static key word

--- staticKeyword.java
+++ staticKeyword.java
@@ -1,7 +1,21 @@
+class Student{
+String name ;
+int age;
+int rollNo;
+static int studentno;
+public  Student(String name,int age, int rollNo){
+    this.name = name;
+    this.age = age;
+    this.rollNo = rollNo;
+    studentno++;
+}
+}
 public class staticKeyword {
     public static void main(String[] args) {
-      int x =44;
-        
+     Student s1 = new Student("sidd", 22, 10);
+        System.out.println(s1.studentno);
+      Student s2 = new Student("suman", 033, 4);
+      System.out.println(s2.studentno);
 
     }
 }
