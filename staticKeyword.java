75172af0364707380f420a4023a4cbb39f76a0e3
Siddharth jain <162619604+godxcodex@users.noreply.github.com>
2025-08-17 21:54:10 +0530

    access mod & getter use

--- staticKeyword.java
+++ staticKeyword.java
@@ -10,16 +10,20 @@ class Student {
         this.rollNo = rollNo;
         studentno++; // increment when a new student is created
     }
+    public void getter(){
+        System.out.println(studentno);
+    }
 }
 
 public class staticKeyword {
     public static void main(String[] args) {
-        Student.studentno =100;// using private access modifire so that the memebers out side the class can not access it 
-        System.out.println(Student.studentno);
+        //Student.studentno =100;// using private access modifire so that the memebers out side the class can not access it 
+        //System.out.println(Student.studentno);
         Student s1 = new Student("Sidd", 22, 10);
-        System.out.println("Total students: " + Student.studentno); 
-
+        //System.out.println("Total students: " + Student.studentno); 
+        
         Student s2 = new Student("Suman", 33, 4);
-        System.out.println("Total students: " + Student.studentno); 
+       // System.out.println("Total students: " + Student.studentno); 
+        s2.getter();
     }
 }
