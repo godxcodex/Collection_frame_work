005f73deed48d2e3850b037ae863a0390b131560
Siddharth jain <162619604+godxcodex@users.noreply.github.com>
2025-08-23 12:29:52 +0530

    print linkedlist

--- List/linkedlist/printing_list.java
+++ List/linkedlist/printing_list.java
@@ -1,5 +1,28 @@
 package List.linkedlist;
 
 public class printing_list {
-    
+    public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(5);//head
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(8);
+        Node e = new Node(16); // tail
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        Node temp = a;
+        while(temp != null){
+           System.out.print(temp.data +" ");
+           temp = temp.next;
+        }
+    }
 }
